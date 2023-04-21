3a68cb2317647e5472516fc07e42177cfb8dde97
lisagomesnrcg <naiaracgomes@gmail.com>
2023-04-20 22:17:57 -0300

    kkkk

--- frontend/AccountOwnerClient/src/app/app.module.ts
+++ frontend/AccountOwnerClient/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
