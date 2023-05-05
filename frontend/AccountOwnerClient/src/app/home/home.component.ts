4152db9bb7be6cb50be263673627b72786df3a6f
lisagomesnrcg <naiaracarolinegomes@gmail.com>
2023-05-04 22:24:40 -0300

    cd.

--- frontend/AccountOwnerClient/src/app/home/home.component.ts
+++ frontend/AccountOwnerClient/src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public homeText: string;
+
+  constructor() { }
+     
+  ngOnInit(): void {
+    this.homeText = "BEM VINDO A APLICAÇÃO ACCOUNT-OWNER"; 
+  }
 }
